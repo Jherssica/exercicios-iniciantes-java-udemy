e24b826658eff995b2f4d8179b3c94b51cf50b62
jherssica <jherssyca@gmail.com>
2022-02-03 15:54:37 -0300

    first commit

---
+++ src/variaveisEstudo/definicaoVariavel.java
@@ -0,0 +1,59 @@
+package variaveisEstudo;
+
+public class definicaoVariavel {
+
+//DEFINIÇÃO DE VARIAVEL
+
+/* Variáveis é uma porção da memoria (RAM) Utilizada para armazenar dados
+durante a execução de um programa.
+
+
+ */
+
+
+//DECLARAÇÃO DA VARIAVEL
+/* SINTAXE:
+<TIPO> <NOME>= <VALOR INICIAL>
+
+
+EXEMPLO:
+ */
+
+ int idade=25;
+ double altura=1.68;
+ char sexo ='f';
+
+ //uma variavel possui um nome,tipo,valor,endereço.
+
+
+/* tipos primitivos em java
+
+tipos numericos inteiros :
+byte=8 bits;
+short= 16 bits;
+int 32 bits;
+long = 64 bits / padrão descritivo no codigo adicionando 
+( L ) no final do valor inicial da variavel/.
+
+tipos  numericos com ponto flutuantes(casas decimais):
+float= 32 bits/ padrão descritivo no codigo adicionando 
+( F ) no final do valor inicial da variavel/.
+double = 64 bits;
+
+UM CARACTER UNICODE:
+char= 16 bits;
+
+
+VALOR VERDADE:
+boolean= 1 bit; {false,true}/valor padrão é false.
+
+String:cadeia de caracteres(palavras ou textos)
+
+NOMES DE VARIAVEIS:
+-NÃO PODE COMEÇAR COM DIGITO;
+-NÃO PODE TER ESPAÇOS EM BRANCO;
+-NÃO USAR ACENTOS OU TIL;
+SUGESTÕES:USE O PADRAO "CAMEL CASE"
+
+ */
+}
