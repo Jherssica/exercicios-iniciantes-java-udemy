47c52f4eba395f40dc8923a7e5241b102c8e4ac3
jherssica <jherssyca@gmail.com>
2022-02-03 16:30:15 -0300

    new commit

---
+++ src/variaveisEstudo/castingDados.java
@@ -0,0 +1,71 @@
+package variaveisEstudo;
+
+public class castingDados {
+
+
+    public static void main(String[] args) {
+        //EXEMPLO 01:
+        int x;
+        int y;
+
+
+        x = 5;
+        y = 2 * x;
+
+
+        System.out.println(x);
+        System.out.println(y);
+
+
+         //EXEMPLO 02:
+
+
+        /*int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+
+        System.out.println(x);
+        System.out.println(y);
+    */
+
+
+        //EXEMPLO 03:
+
+
+        double b,B,h, area;
+
+        b=6.0;
+        B= 8.0;
+        h= 5.0;
+
+        area= (b+B) / 2.0 * h;
+
+        System.out.println(area);
+
+        /*BOA PRATICA:
+         SEMPRE INDIQUE O TIPO DE NUMERO,
+         SE A EXPRESSÃO FOR DE PONTO FLUTUANTE(NÃO INTEIRA)
+
+         PARA DOUBLE USE:
+         .0
+
+         PARA FLOAT USE:
+         F
+         */
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
